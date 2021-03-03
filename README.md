44d976ee75448f461b4df8cb4432ac4aec4703e4
Samuel-Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-03-03 18:02:07 +0000

    [main] + LocalStorage to log Edits

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 A To-do list App to promote wellbeing and mindfulness activities
-
+https://samuel-morgan-tyghe.github.io/Midwifery-Toolkit-TodoList/
--- src/App.js
+++ src/App.js
@@ -2,23 +2,58 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useState } from "react";
+import React from "react";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Have A Bath",
-      day: "Tommorow Evening",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Go for a Walk",
-      day: "Weekend",
-      reminder: true,
-    },
-  ]);
+
+  const [name, setName] = useLocalStorage("tasks", () => "");
+
+  const stickyValue = window.localStorage.getItem("tasks");
+
+  const [tasks, setTasks] = useState(
+    stickyValue
+      ? name
+      : [
+          {
+            id: 1,
+            text: "Have A Bath",
+            day: "Tommorow Evening",
+            reminder: true,
+          },
+          {
+            id: 2,
+            text: "Go for a Walk",
+            day: "Weekend",
+            reminder: true,
+          },
+        ]
+  );
+
+  function useLocalStorage(key, initialValue) {
+    const [storedValue, setStoredValue] = useState(() => {
+      try {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      } catch (error) {
+        console.log(error);
+        return initialValue;
+      }
+    });
+
+    const setValue = (value) => {
+      try {
+        const valueToStore =
+          value instanceof Function ? value(storedValue) : value;
+        setStoredValue(valueToStore);
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return [storedValue, setValue];
+  }
 
   //Add Task
 
@@ -26,17 +61,28 @@ function App() {
     const id = Math.floor(Math.random() * 10000) + 1;
     const newTask = { id, ...task };
     setTasks([...tasks, newTask]);
+    setName([...tasks, newTask]);
+    console.log(
+      "🚀 ~ file: App.js ~ line 76 ~ addTask ~ (tasks.push(newTask))",
+      [...tasks, newTask]
+    );
   };
 
   //delete task
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
+    setName(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle Reminder
   const toggleReminder = (id) => {
     setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+    setName(
       tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
       )
